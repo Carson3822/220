5ffbd5606f26d20f6056dec8307a85440fa98fec
carson3822 <shieldsca@g.cofc.edu>
2022-01-26 15:48:48 -0500

    completed lab2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,69 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <your name goes here – Carson Shields
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Problem: Homework 2 problems
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    ubound = eval(input("what is the upper bound?: "))
+    sum3 = 0
+    for i in range(0, ubound):
+        if (i*3) <= ubound:
+            sum3 += i*3
+
+
+    print(sum3)
+
 
 
 def multiplication_table():
-    pass
+    for x_1 in range( 1 , 11 ):
+        for y_1 in range( 1 , 11 ):
+            print(str(x_1 * y_1), end="\t")
+        print()
 
 
 def triangle_area():
-    pass
+    length_a = eval(input("enter side a length: "))
+    length_b = eval(input("enter side b length: "))
+    length_c = eval(input("enter side c length: "))
+
+    s_1 = ((length_a + length_b + length_c)/ 2)
+
+    tot_area = math.sqrt(s_1*((s_1-length_a)*(s_1-length_b)*(s_1-length_c)))
+
+    print(tot_area)
+
 
 
 def sum_squares():
-    pass
+    low_lim = eval(input("Enter lower range: "))
+    up_lim = eval(input("Enter upper range: "))
+
+    sum_1 = 0
+    for i in range(low_lim, up_lim+1):
+        sum_1 += (i ** 2)
+
+    print(sum_1)
 
 
 def power():
-    pass
+    base = eval(input("Enter Base:"))
+    pow_ = eval(input("Enter Power: "))
+
+    answer = base ** pow_
+
+    print(answer)
+
+
+
+
 
 
 if __name__ == '__main__':
